bd6d664a26a271f3890cecea4420addd5550238c
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-06-01 08:54:04 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,9 +1,10 @@
-get('text').on_submit(function(content)
-  get("results").set_content('search')
-  local res = fetch({
-    url = 'http://eu2.host.hitbyathunder.xyz:20420/search?q=' .. content,
-    method = "GET"
-  })
-  print(content)
-  get("results").set_content(res)
-end)
+get("results").set_content('test')
+--get('text').on_submit(function(content)
+--  get("results").set_content('search')
+--  local res = fetch({
+--    url = 'http://eu2.host.hitbyathunder.xyz:20420/search?q=' .. content,
+--    method = "GET"
+--  })
+--  print(content)
+--  get("results").set_content(res)
+--end)

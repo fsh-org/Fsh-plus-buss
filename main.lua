b43714cb614a2a4fe1292d795457f493548eb769
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-06-02 13:50:28 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,23 +1,22 @@
 get("results").set_opacity(0)
 
 get('text').on_submit(function(content)
-  local favicon = get('favicon', true)
   local url = get('url', true)
   local title = get('title', true)
   local desc = get('desc', true)
   get("status").set_content('searching...')
   local res = fetch({
-    url = 'https://api.fsh.plus/images/data/dingle.json',
+    url = 'https://api.fsh.plus/buss?q='..content,
     method = "GET",
     headers = { ["Content-Type"] = "text/plain" },
     body = 'Text'
   })
   get("status").set_content('loaded')
   get("status").set_content(#res .. ' results')
---  for k, v in res do
---    favicon[k].set_source(v['favicon'])
---    url[k].set_href('buss://' .. v['url'])
---    title[k].set_content(v['title'])
---    desc[k].set_content(v['desc'])
---  end
+  for k, v in res do
+    favicon[k].set_source(v['favicon'])
+    url[k].set_href('buss://' .. v['url'])
+    title[k].set_content(v['title'])
+    desc[k].set_content(v['text'])
+  end
 end)

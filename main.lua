61dddf2a808979e773fe9fb69e7a56f04949a966
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-06-04 21:37:56 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -17,7 +17,9 @@ get('text').on_submit(function(content)
   get("status").set_content(#res .. ' results')
   get("results").set_opacity(1)
   for k = 1, #res do
+    print(k)
     local v = res[k]
+    print(v)
     url[k].set_href('buss://' .. v['url'])
     title[k].set_content(v['title'])
     desc[k].set_content(v['text'])

764b62f13045ca7e6cddd7ecff1f7eb9b7ff7d12
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-06-04 16:18:21 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 get("results").set_opacity(0)
 
 get('text').on_submit(function(content)
+  print('search')
   local url = get('url', true)
   local title = get('title', true)
   local desc = get('desc', true)
@@ -14,7 +15,9 @@ get('text').on_submit(function(content)
   })
   get("status").set_content('loaded')
   get("status").set_content(#res .. ' results')
-  for k, v in pairs(res) do
+  print('search')
+  for k = 1, #res do
+    local v = res[k]
     favicon[k].set_source(v['favicon'])
     url[k].set_href('buss://' .. v['url'])
     title[k].set_content(v['title'])

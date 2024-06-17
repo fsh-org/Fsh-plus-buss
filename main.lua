5032f3a79837b9cd4dcb13c6615cb8ccf8e52b5d
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-06-17 11:10:56 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -17,7 +17,7 @@ get('text').on_submit(function(content)
   get("results").set_opacity(1)
   for k = 1, #res do
     local v = res[k]
-    #title[k].set_href('buss://' .. v['url'])
+    --title[k].set_href('buss://' .. v['url'])
     title[k].set_content(v['title'] .. v['url'])
     desc[k].set_content(v['text'])
   end
